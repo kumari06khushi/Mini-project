2dc27e02f8e930a4f95f4bbc35a0f1db30aa3ae1
Khushi Kumari <khushi16bce@gmail.com>
2026-06-05 18:52:28 +0530

    upload portfolio.js

---
+++ My-portfolio/portfolio.js
@@ -0,0 +1,16 @@
+var tablinks=document.getElementsByClassName("tab-links");
+var tabcontents=document.getElementsByClassName("tab-contents");
+function opentab(tabname,event){
+  for( let tablink of  tablinks) {
+    tablink.classList.remove("active-link");
+  } 
+  for(let tabcontent of  tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link")
+  document.getElementById(tabname).classList.add("active-tab");
+}
+function toggleMenu() {
+  const nav = document.querySelector("nav ul");
+  nav.classList.toggle("show");
+}
